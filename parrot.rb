b44e712ebe7de06cebb3195403e04122be010604
steve <stevens714@yahoo.com>
2020-09-29 16:15:50 -0400

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot(name = "Squawk!")
+    puts "#{name}"
+    return name
+    #in ruby methods, there are implicit returns which means that if you don't explicitly use the return keyword, the last line of code is the return value 
+end
+
+# def add_two(num)
+#     puts num + 2
+#     num + 2
+# end 
+
+# sum = add_two(10)
